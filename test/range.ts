18ecf1f5d3e176475a682b26a86a87f2b9a89679
haegul <phg2491@gmail.com>
2018-10-23 23:50:18 +0900

    update range test case (#2)

--- test/range.ts
+++ test/range.ts
@@ -27,9 +27,34 @@ describe('Test Range API', () => {
             expect(iter.next()).to.deep.equal({value: 0, done: false});
             expect(iter.next()).to.deep.equal({value: 5, done: true});
         });
+
+        it('decrease number 1', () => {
+            const iter = range(5, 0, 1);
+            expect(iter.next()).to.deep.equal({value: 5, done: false});
+            expect(iter.next()).to.deep.equal({value: 4, done: false});
+            expect(iter.next()).to.deep.equal({value: 3, done: false});
+            expect(iter.next()).to.deep.equal({value: 2, done: false});
+            expect(iter.next()).to.deep.equal({value: 1, done: false});
+            expect(iter.next()).to.deep.equal({value: 0, done: true});
+        });
+
+        it('decrease number 2', () => {
+            const iter = range(5, 0, 1);
+            expect(iter.next()).to.deep.equal({value: 5, done: false});
+            expect(iter.next()).to.deep.equal({value: 3, done: false});
+            expect(iter.next()).to.deep.equal({value: 1, done: true});
+        });
+
+        it('decrease number 5', () => {
+            const iter = range(5, 0, 5);
+            expect(iter.next()).to.deep.equal({value: 5, done: false});
+            expect(iter.next()).to.deep.equal({value: 0, done: true});
+        });
     });
 
     describe('edge case', () => {
-
+        it('Over step the range', () => {
+            const iter = range(0, 1, 2);
+        });
     });
 });
