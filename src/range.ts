cde13f576c02cb4937703988a59fd1aec884fd9d
haegul <phg2491@gmail.com>
2018-10-27 19:02:00 +0900

    Support range step function type (#3)

--- src/range.ts
+++ src/range.ts
@@ -1,26 +1,26 @@
-function* range(start: number, end: number, step: number): any {
+function* range(start: number, end: number, step: any): any {
     if (start === end) {
         throw new Error('The start and end parameter is same.');
     }
 
     let current = start;
-    let finished: number;
-    let checker: Function;
+    let checker;
+    let next = step;
 
-    let next = (value: number) => {
-        return value + step;
-    };
+    if (typeof step === 'number') {
+        next = (value: number) => {
+            return value + step;
+        };
+    }
 
     if (start < end) {
         checker = (value: number) => {
-            return value < finished;
+            return next(value) < end;
         };
-        finished = end - 1;
     } else {
         checker = (value: number) => {
-            return value > finished;
+            return next(value) > end;
         };
-        finished = end + 1;
     }
 
     while (checker(current)) {
--- test/range.ts
+++ test/range.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { range } from '../src';
 
 describe('Test Range API', () => {
-    describe('Default case', () => {
+    describe('step is number', () => {
         it('increase number 1', () => {
             const iter = range(0, 5, 1);
             expect(iter.next()).to.deep.equal({ value: 0, done: false });
@@ -45,6 +45,33 @@ describe('Test Range API', () => {
             const iter = range(5, 0, -4);
             expect(iter.next()).to.deep.equal({ value: 5, done: false });
             expect(iter.next()).to.deep.equal({ value: 1, done: true });
+        });
+    });
+
+    describe('Step is Function', () => {
+        it('increase number 1', () => {
+            const iter = range(0, 5, (x: number) => x + 1);
+            expect(iter.next()).to.deep.equal({ value: 0, done: false });
+            expect(iter.next()).to.deep.equal({ value: 1, done: false });
+            expect(iter.next()).to.deep.equal({ value: 2, done: false });
+            expect(iter.next()).to.deep.equal({ value: 3, done: false });
+            expect(iter.next()).to.deep.equal({ value: 4, done: true });
+        });
+
+        it('decrease number 1', () => {
+            const iter = range(5, 0, (x: number) => x - 1);
+            expect(iter.next()).to.deep.equal({ value: 5, done: false });
+            expect(iter.next()).to.deep.equal({ value: 4, done: false });
+            expect(iter.next()).to.deep.equal({ value: 3, done: false });
+            expect(iter.next()).to.deep.equal({ value: 2, done: false });
+            expect(iter.next()).to.deep.equal({ value: 1, done: true });
+        });
+
+        it('square x^2', () => {
+            const iter = range(2, 64, (x: number) => x * x);
+            expect(iter.next()).to.deep.equal({ value: 2, done: false });
+            expect(iter.next()).to.deep.equal({ value: 4, done: false });
+            expect(iter.next()).to.deep.equal({ value: 16, done: true });
         });
     });
 
