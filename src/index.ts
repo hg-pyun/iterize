8f9076efb41af485c226085296c3f61ca8a54ff5
haegul <phg2491@gmail.com>
2018-12-16 14:28:43 +0900

    Remove replicate API (#45)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import range from './range';
 import cycle from './cycle';
 import take from './take';
 import repeat from './repeat';
-import replicate from './replicate';
 
-export { range, cycle, take, repeat, replicate };
+export { range, cycle, take, repeat };
