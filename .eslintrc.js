3a84919e6b6d018cd0a52ae778b3ffe0063e36c7
haegul <phg2491@gmail.com>
2018-12-28 18:22:01 +0900

    Remove Prettier, adjust eslint (#53)

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "parser": "typescript-eslint-parser",
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "single"],
+        "indent": ["error", 4],
+        "comma-dangle": ["error", "always-multiline"],
+        "no-unused-vars": 0
+    }
+};
