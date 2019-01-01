af54df91ebeb96351e6f2573883aac40d7ccb289
haegul <phg2491@gmail.com>
2019-01-01 15:38:08 +0900

    Add UMD config and file (#56)

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,21 @@ module.exports = {
     entry: path.resolve(__dirname, './src/index.ts'),
     output: {
         filename: 'iterize.min.js',
-        path: path.resolve(__dirname, 'lib/bundle')
+        path: path.resolve(__dirname, 'lib/bundle'),
+        library: 'Iterize',
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                resolve: {extensions: ['.ts']},
+                resolve: {extensions: ['.ts', '.js']},
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-typescript'],
                         plugins: [
-                            ["@babel/plugin-transform-modules-umd", {
-                                "globals": {
-                                    "iterize": "Iterize"
-                                },
-                                "exactGlobals": true
-                            }],
                             "@babel/plugin-transform-runtime",
                             "@babel/plugin-proposal-class-properties"
                         ]
@@ -31,8 +27,5 @@ module.exports = {
                 }
             }
         ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
+    }
 };
